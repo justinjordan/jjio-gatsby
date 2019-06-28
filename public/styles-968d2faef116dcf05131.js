07c59680cfdc7faa6bc318db1ec34eb440e11807
Justin Jordan <jsxule@gmail.com>
2019-06-27 23:28:09 -0500

    Actually don't commit build

--- public/styles-968d2faef116dcf05131.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{265:function(n,w,o){}}]);
-//# sourceMappingURL=styles-968d2faef116dcf05131.js.map
--- public/styles-f8bfbeea588301730ff1.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{299:function(n,w,o){}}]);
-//# sourceMappingURL=styles-f8bfbeea588301730ff1.js.map
